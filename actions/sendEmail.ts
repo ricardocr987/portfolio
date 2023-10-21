9ce41f8586096ff412df623837324243dbc3344c
ricardoc987 <ricardocr987@gmail.com>
2023-10-21 20:44:06 +0200

    update web mail

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@ricardocr987.com>",
+      from: "Contact Form <onboarding@riki.bio>",
       to: "ricardocr987@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
