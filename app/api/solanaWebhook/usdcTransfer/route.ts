3a740ae6a3f46828c8a56c0d0a1209cecc6e4766
ricardoc987 <ricardocr987@gmail.com>
2023-11-07 21:57:20 +0100

    add spl transfer callback

---
+++ app/api/solanaWebhook/usdcTransfer/route.ts
@@ -0,0 +1,51 @@
+import config from "@/lib/env";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { BodyRequest } from "./schema";
+import bs58 from "bs58";
+import { decryptData } from "@/lib/encrypt";
+import { DateProps } from "@/app/meeting/types";
+import { formatDateProps, generateMeet } from "@/lib/meet";
+
+const requestBodySchema = z.array(BodyRequest);
+
+export async function POST(req: NextRequest) {
+    const authorization = req.headers.get('Authorization');
+    if (authorization !== `Bearer ${config.SOLANA_WEBHOOK_AUTH}`) {
+        console.log('Unauthorized request');
+        return new Response('Unauthorized request', { status: 401 });
+    }
+
+    const requestBody = requestBodySchema.parse(req.body);
+    console.log('Received request body:', requestBody);
+
+    const asyncTasks = requestBody.map(async (rawTxn) => {
+        try {
+            const { transaction } = rawTxn;
+            const instructionData = transaction.message.instructions[1].data;
+            const bufferData = Buffer.from(bs58.decode(instructionData));
+            const data = decryptData(bufferData.toString());
+            const { date, customerEmail, sessionId, message } = JSON.parse(data);
+            const dateProps: DateProps = date;
+            const meetingTime = formatDateProps(dateProps);
+
+            try {
+                await generateMeet(meetingTime, customerEmail, sessionId, message);
+                console.log('Generated meeting:', meetingTime);
+            } catch (error) {
+                console.error('Webhook Error:', error);
+                return NextResponse.json(JSON.stringify({ error }));
+            }
+          
+            return NextResponse.json(JSON.stringify({ received: true }));
+        } catch (error) {
+            console.error('Webhook Error:', error);
+            return NextResponse.json(JSON.stringify({ error: 'Stripe error' }));
+        }
+    });
+
+    const results = await Promise.all(asyncTasks);
+    const transactionIds = results.map((result) => result).join(", ");
+    console.log('Transaction IDs:', transactionIds);
+    return new Response(`IDs: ${transactionIds} added.`, { status: 200 });
+}
