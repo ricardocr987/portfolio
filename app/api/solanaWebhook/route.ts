480e6a8fc2193fa1d07765ecf521cf597b2498e0
ricardoc987 <ricardocr987@gmail.com>
2023-11-08 23:33:38 +0100

    idiota

--- app/api/solanaWebhook/route.ts
+++ app/api/solanaWebhook/route.ts
@@ -8,7 +8,7 @@ import config from "@/lib/env";
 export async function POST(req: NextRequest) {
     try {
         const authorization = req.headers.get('x-api-key');
-        if (authorization == config.SOLANA_WEBHOOK_AUTH) {
+        if (authorization !== config.SOLANA_WEBHOOK_AUTH) {
             throw new Error('Invalid authcallback request');
         }
         /*if (authorization !== `Bearer ${config.SOLANA_WEBHOOK_AUTH}`) {
